3652193ca047a3b4e6c2e1583711bef424c0de32
Daniil <www.daniil_.ru@mail.ru>
2022-09-07 23:32:38 +0700

    add dist

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	publicPath: process.env.NODE_ENV === "production" ? "/strategy-game-vue/" : "/",
+	css: {
+		loaderOptions: {
+			sass: {
+				prependData: ' @import "@/styles/_variables.scss"; '
+			}
+		}
+	}
+}
